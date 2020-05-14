aafe7d564c1984ad2c9806f91032d505e7e609a4
bsn <62386363+43117425@users.noreply.github.com>
2020-05-14 21:06:31 +0800

    teat

--- project-main.c
+++ project-main.c
@@ -6,33 +6,37 @@ sbit led_run=P0^1;
 sbit beef=P0^2;
 sbit engine=P1^0;
 
-unsigned i;
 void delay_microsecond(unsigned microsec)
 {
-    for (i = 0; i < microsec; i++);
+    unsigned i;
+	for (i = 0; i < microsec; i++);
     return;
 }
 void delay_millisecond(unsigned millisec)
 {
-    for (i = 0; i < millisec; i++)
+    unsigned i;
+	for (i = 0; i < millisec; i++)
         delay_microsecond(1000);
     return;
 }
 void delay_second(unsigned second)
 {
-    for (i = 0; i < second; i++)
+    unsigned i;
+	for (i = 0; i < second; i++)
         delay_millisecond(1000);
     return;
 }    
 void delay_minute(unsigned minute)    
 {
-    for (i = 0; i < minute; i++)
+    unsigned i;
+	for (i = 0; i < minute; i++)
         delay_second(60);
     return;
 }
 void delay_hour(unsigned hour)
 {
-    for (i = 0; i < hour; i++)
+    unsigned i;
+	for (i = 0; i < hour; i++)
         delay_minute(60);
     return;
 }
@@ -51,7 +55,7 @@ for(j=0;j<600;j++)
 
 engine=1;
 led_run=0;
-delay_second(20);
+delay_second(5);
 engine=0;
 led_run=1;
 
